a07877c77d993a6279699a6628c4697afc177a80
Joshua Wood <JerichoWall@apu.edu>
2019-03-08 08:21:38 -0800

    Basic setup and dependencies done got

--- smurfs/src/reducers/rootReducer.js
+++ smurfs/src/reducers/rootReducer.js
@@ -13,6 +13,14 @@
    error: null
  }
 */
+const initialState = {
+  smurfs: [],
+  fetchingSmurfs: false,
+  addingSmurf: false,
+  updatingSmurf: false,
+  deletingSmurf: false,
+  error: null
+}
 
 /*
   You'll only need one smurf reducer for this project.
@@ -21,3 +29,11 @@
   There is no need for 'combineReducers' in this project.
   Components can then read your store as, `state` and not `state.fooReducer`.
 */
+const rootReducer = (state = initialState, action) => {
+    switch(action.type) {
+
+      default: return state;
+    }
+}
+
+export default rootReducer;
